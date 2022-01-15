c7813a5eca2ff2187be88800d659e0c5fef786cd
BB <bb@test.bb>
2022-01-15 20:54:45 +0100

    Simplify keybindings

--- .dot/zsh/plugins.d/zsh-history-substring-search.zsh
+++ .dot/zsh/plugins.d/zsh-history-substring-search.zsh
@@ -10,14 +10,3 @@ HISTORY_SUBSTRING_SEARCH_ENSURE_UNIQUE=set
 # Bind ^[[A/^[[B for history search after sourcing the file
 bindkey '^[[A' history-substring-search-up
 bindkey '^[[B' history-substring-search-down
-
-OS=$(uname -s)
-if [ "$OS" = "Darwin" ]; then
-    bindkey '^[[A' history-substring-search-up
-    bindkey '^[[B' history-substring-search-down
-elif [ "$OS" = "Linux" ]; then
-    # https://superuser.com/a/1296543
-    # key dict is defined in /etc/zsh/zshrc
-    bindkey "$key[Up]" history-substring-search-up
-    bindkey "$key[Down]" history-substring-search-down
-fi
