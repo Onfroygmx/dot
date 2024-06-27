37259d67cfc548976172a0c60bf7d15197a1695b
Onf <onfroy@gmx.com>
2024-06-27 21:13:24 +0200

    Modify on settings

--- .dot/zsh/modules/debug/debug_on.zsh
+++ .dot/zsh/modules/debug/debug_on.zsh
@@ -10,11 +10,18 @@ if [[ "$ZSH_PROFILE" = true ]]; then
   [[ ! -d "$ZSH_CACHE_DIR/zprof" ]] && mkdir -p "$ZSH_CACHE_DIR/zprof"
   zmodload zsh/zprof
 fi
+
+#
+# [[ ! -d "$ZSH_CACHE_DIR/zprof" ]] && mkdir -p "$ZSH_CACHE_DIR/zprof" 
+# (( ${ZSH_PROFILE:-0} )) && zmodload zsh/zprof
+#
+
 #
 # Ztrace
 #
 # To launch:
 # ZSH_XTRACE=true zsh
+# if (( ${ZSH_XTRACE:-0} )); then
 if [[ "$ZSH_XTRACE" = true ]]; then
   [[ ! -d "$ZSH_CACHE_DIR/xtrace" ]] && mkdir -p "$ZSH_CACHE_DIR/xtrace"
   (( ${+EPOCHREALTIME} )) || zmodload zsh/datetime
